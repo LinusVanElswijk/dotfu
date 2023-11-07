be34e91b3cbe987cf87d144ee374535c307c087d
Linus van Elswijk <linusvanelswijk@thirona.eu>
2023-11-07 17:18:12 +0100

    dotfu: Automatic loading of environment_variables.dotfu files in ~/.config.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,8 +8,14 @@ dotfu_conf_dir="${HOME}/.config/dotfu"
 echo "Generating dotfu config in ${dotfu_conf_dir}."
 mkdir -p $dotfu_conf_dir
 echo "export DOTFILES_DIR=${DOTFILES_DIR}" > "${dotfu_conf_dir}/environment_variables"
-echo "Done"
 
 echo "Running 'dotfu install bash'..."
 "${DOTFILES_DIR}"/scripts/dotfu install bash
+
+echo "Putting a link to dotfu in ${HOME}/bin."
+link_dest="${HOME}/bin/dotfu"
+if [[ -L ${link_dest} ]] ; then
+  rm $link_dest
+fi
+ln -s $DOTFILES_DIR/scripts/dotfu $link_dest
 echo "Done."
