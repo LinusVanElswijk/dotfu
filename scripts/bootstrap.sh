9e90411eff1d02018de931b8f85a1cf7fc29198d
Linus van Elswijk <linusvanelswijk@thirona.eu>
2023-11-07 16:43:36 +0100

    dotfu: Fix issue with DOTFILES_DIR.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,7 +4,12 @@
 # Bootstrap is always directly in the root dir.
 export DOTFILES_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-echo "Running 'dotfu install bash'..."
-"${DOTFILE_DIR}"/scripts/dotfu install bash
-echo "Done."
+dotfu_conf_dir="${HOME}/.config/dotfu"
+echo "Generating dotfu config in ${dotfu_conf_dir}."
+mkdir -p $dotfu_conf_dir
+echo "export DOTFILES_DIR=${DOTFILES_DIR}" > "${dotfu_conf_dir}/environment_variables"
+echo "Done"
 
+echo "Running 'dotfu install bash'..."
+"${DOTFILES_DIR}"/scripts/dotfu install bash
+echo "Done."
